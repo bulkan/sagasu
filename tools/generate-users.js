add03333725e6625796371a1abfd0bdf7df965fe
Bulkan Evcimen <bulkan@gmail.com>
2019-12-09 18:13:48 +1100

    Script to generate a large users.json

---
+++ tools/generate-users.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const faker = require('faker');
+const JSONStream = require('JSONStream');
+
+const generateUser = () => {
+  const id = faker.random.number();
+  const name = faker.name.findName();
+
+  return {
+    _id: id,
+    url: `http://initech.zendesk.com/api/v2/users/${id}.json`,
+    external_id: faker.random.uuid(),
+    name,
+    alias: name,
+    created_at: faker.date.past(),
+    active: faker.random.boolean(),
+    verified: faker.random.boolean(),
+    shared: faker.random.boolean(),
+    locale: faker.random.locale(),
+    timezone: faker.address.city(),
+    last_login_at: faker.date.recent(),
+    email: faker.internet.email(),
+    phone: faker.phone.phoneNumber(),
+    signature: `Don't Worry Be Happy!`,
+    organization_id: faker.random.number(),
+    tags: [
+      'Springville',
+      'Sutton',
+      'Hartsville/Hartley',
+      'Diaperville'
+    ],
+    suspended: faker.random.boolean(),
+    role: 'admin'
+  };
+}
+
+
+function main() {
+  const transformStream = JSONStream.stringify();
+  const outputStream = fs.createWriteStream(`${__dirname}/large-users.json`);
+
+  transformStream.pipe(outputStream);
+
+  const max = 100000;
+
+  for (let i = 0; i <= max; i++) {
+    console.log(`Generating user ${i++} / ${max}`);
+    const user = generateUser();
+    transformStream.write(user);
+  }
+
+  transformStream.end();
+
+  outputStream.on('finish', () => console.log('done'));
+}
+
+if (require.main === module) {
+  main();
+}
